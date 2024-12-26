e5542661a023298544509a7d9e20226b71fe34a6
jjservice <jjfabian47@gmail.com>
2024-12-26 13:48:16 -0500

    Uius65

---
+++ Contact.js
@@ -0,0 +1,68 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCBDffMAaJdgb1_Xsfa4zhoYLYAhxmrZVo",
+    authDomain: "contact-form-9868a.firebaseapp.com",
+    databaseURL: "https://contact-form-9868a-default-rtdb.firebaseio.com",
+    projectId: "contact-form-9868a",
+    storageBucket: "contact-form-9868a.firebasestorage.app",
+    messagingSenderId: "686565340119",
+    appId: "1:686565340119:web:8cbc0e2cfc7772af449192"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference your database
+  var contactFormDB = firebase.database().ref("contactForm");
+  
+  document.getElementById("contactForm").addEventListener("submit", submitForm);
+  
+  function submitForm(e) {
+    e.preventDefault();
+  
+    var name = getElementVal("name");
+    var emailid = getElementVal("emailid");
+    var phone = getElementVal("phone");
+  
+    saveMessages(name, emailid, phone);
+  
+    // Enable alert
+    document.querySelector(".alert").style.display = "block";
+  
+    // Remove the alert after 3 seconds
+    setTimeout(() => {
+      document.querySelector(".alert").style.display = "none";
+    }, 3000);
+  
+    // Redirect to another page after the form submission
+    setTimeout(() => {
+      window.location.href = "https://chatgpt.com/";  // Replace with your desired URL
+    }, 3000); // Delay to allow alert to disappear before redirecting
+  
+    // Reset the form
+    document.getElementById("contactForm").reset();
+  }
+  
+  const saveMessages = (name, emailid, phone) => {
+    var newContactForm = contactFormDB.push();
+  
+    newContactForm.set({
+      name: name,
+      emailid: emailid,
+      phone: phone,
+    });
+  };
+  
+  const getElementVal = (id) => {
+    return document.getElementById(id).value;
+  };
+  
+
+
+
+  ////Light Section/////
+
+  function toggleLight(){
+    const body = document.querySelector('body');
+    body.classList.toggle('light');
+  }
